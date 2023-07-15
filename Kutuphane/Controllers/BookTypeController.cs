65ca5335040e18e89b02ee1c5ea6aadf654a9dc1
Burak Pişkin <burak.12000@hotmail.com>
2023-07-15 18:04:48 +0300

    implement booktype mvc

---
+++ Kutuphane/Controllers/BookTypeController.cs
@@ -0,0 +1,21 @@
+﻿using Kutuphane.Models;
+using Kutuphane.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kutuphane.Controllers
+{
+    public class BookTypeController : Controller
+    {
+        private readonly LibraryDbContext _libraryDbContext;
+
+        public BookTypeController(LibraryDbContext ctx)
+        {
+            _libraryDbContext = ctx;
+        }
+        public IActionResult Index()
+        {
+            List<BookType> bookTypes = _libraryDbContext.bookTypes.ToList();
+            return View(bookTypes);
+        }
+    }
+}
